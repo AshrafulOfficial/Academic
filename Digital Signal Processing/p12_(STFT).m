fc488abe8f16a43e00b0130c2405da87a4586c6a
ashraful <ashrafulofficialc@gmail.com>
2026-07-15 23:25:40 +0600

    ok

--- Digital Signal Processing/p12_(STFT).m
+++ Digital Signal Processing/p12_(STFT).m
@@ -3,7 +3,10 @@ t = 0:1/fs:2;
 
 x1 = sin(2*pi*100*t);
 
-x2 = [sin(2*pi*50*t(1:end/2)) sin(2*pi*200*t(end/2+1:end))];
+mid = floor(length(t)/2);
+
+x2 = [sin(2*pi*50*t(1:mid)) ...
+      sin(2*pi*200*t(mid+1:end))];
 
 subplot(2,1,1)
 spectrogram(x1,128,120,128,fs,'yaxis')
--- Digital Signal Processing/test.m
+++ Digital Signal Processing/test.m
@@ -9,7 +9,7 @@ xr = interp1(ts, xs, t, 'linear');
 
 Uniform Quantization:
 xq = round((x - min(x))/delta)*delta + min(x);
-delta = (maz(x) - min(x))/2^b;
+delta = (maz(x) - min(x))/(2^b -1);
 error = x - xq;
 Mean Squared Error(MSE): mse1 = mean(error.^2);
 Printing: at last just write mse1.
@@ -20,6 +20,9 @@ Variance - var(x);
 SD - std(x);
 Skewness - skewness(x);
 Kurtosis - kurtosis(x);
+
+output print:
+display(variable)
 
 Correlation and Covariance: x=[1 2 3 4 5]; y=[2 4 6 8 10]
 Auto Correlation - xcorr(x);
